19bb773af8ddf86983b1e6ed0c1d9d71a25e68f4
Mr4K5 <113898911+ryantzyryan@users.noreply.github.com>
2022-09-21 15:02:51 +0700

    Add files via upload

---
+++ bash lobby.sh
@@ -0,0 +1,22 @@
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+NOW=`date "+%d.%m.%Y"`
+TIME=`date "+%H:%M"`
+cowsay -f eyes *Welcome To Names hacking tool* | lolcat
+toilet -f standard " *Mr4K5* " -F gay
+neofetch
+echo " Time : " $TIME | lolcat
+date | lolcat
+echo
+echo Username : ryantzyryan | lolcat
+echo Hostname : @Mr4K5~#root_system | lolcat
+echo
+echo @localhost:~# | lolcat
+echo root@localhost:~$ | lolcat
+echo -e $green
+echo root@MrK5:~$
